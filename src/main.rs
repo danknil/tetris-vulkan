a4fdaf2871492c0dd8798c976c9ca74797cbf512
danknil <bmisa6233@gmail.com>
2020-10-20 20:26:44 +0700

    cringe adding some code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-mod library;
+mod Models;
+mod Render;
 
+use crate::Models::{figure::AbstractFigure, map::TetrisMap};
 use std::sync::Arc;
 
 use winit::{event::WindowEvent, event_loop::EventLoop};
 
 use vulkano::{
-    device::{Device, Queue},
+    device::{Device, Features, Queue, RawDeviceExtensions},
     instance::{Instance, InstanceExtensions, PhysicalDevice},
 };
 
@@ -17,5 +19,31 @@ fn main() {
     )
     .expect("failed to create vulkan instance");
 
-    let physdevs: Vec<String> = library::get_phys_vec(&instance).unwrap();
+    let physdev = PhysicalDevice::enumerate(&instance)
+        .next()
+        .expect("physical device doesn't exist");
+
+    println!(
+        "Using device named: {}\nType: {:?}",
+        physdev.name(),
+        physdev.ty(),
+    );
+
+    let queue_families = [physdev
+        .queue_families()
+        .find(|q| q.supports_graphics())
+        .unwrap()];
+
+    let (device, mut queue) = Device::new(
+        physdev,
+        &Features::default(),
+        RawDeviceExtensions::supported_by_device(physdev),
+        queue_families.iter().cloned().zip([0.5].iter().cloned()),
+    )
+    .expect("can't make device");
+}
+
+fn init_map() -> TetrisMap {
+    let figures: Vec<AbstractFigure> = vec![];
+    TetrisMap::new(figures, 10, 10, 20)
 }
